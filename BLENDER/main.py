72deeeceb289e9649ee54f9b687ccb897790d49f
mariofpalos <marioferpa@gmail.com>
2021-12-09 00:23:40 +0100

    Found a good way of running an external .py from inside Blender

--- BLENDER/main.py
+++ BLENDER/main.py
@@ -1,6 +1,3 @@
-
-celestialRadius = 1 # Gotta decide this value.
-
 originalStar = bpy.data.objects['Star'].data # input mesh. Should it be a lamp? Or both?
 
 scene = bpy.context.scene
@@ -12,5 +9,35 @@ ob = bpy.data.objects.new('Duplicate_Linked', originalStar)
 scene.collection.objects.link(ob)
 
 
-# more code if you want...
+# Next: import the csv into a pandas dataframe
+
+import pandas as pd
+
+#df = pd.read_csv("low_res_euclid_gaia_map_v1.csv", index_col = 0) # Can't find this from blender
+
+fullPath = "/home/mario/Work/Sparkles/BLENDER/low_res_euclid_gaia_map_v1.csv"
+df = pd.read_csv(fullPath, index_col = 0) # Can't find this from blender
+print(df.head())
+
+# Drop some columns.
+df = df.drop(columns=["ra", "dec", "ipix"])
+print(df.head())
+
+# Now convert to cartesian
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
+# Testing with first coordinates
+raTest = df.iloc[0]["ra_bary"] * u.degree
+decTest = df.iloc[0]["dec_bary"] * u.degree
+celestialSphereRadius = 100000000 * u.km
+
+c = SkyCoord(ra = raTest, dec = decTest, distance = celestialSphereRadius, frame='icrs') # ICRS? Sure?
+
+x = c.cartesian.x
+y = c.cartesian.y
+z = c.cartesian.z
+
+print(x, y, z)
+
 ob.location = (2,2,2) # place it somewhere else...
